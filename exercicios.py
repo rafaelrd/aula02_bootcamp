478d1e19dae4687346cc4b3baecafc89b322b6f0
rafael <rafael.ribeiro@a4pm.com.br>
2025-01-10 16:24:55 -0300

    Adicionado exercicio 14

--- exercicios.py
+++ exercicios.py
@@ -1,5 +1,6 @@
 import math
 
+# Exercicio 04: Divisão Inteira do Primeiro pelo Segundo Número
 """ 
 numero01 = int(input("Informe um número inteiro: "))
 numero02 = int(input("Informe outro número inteiro: "))
@@ -7,6 +8,16 @@ resultado = numero01 // numero02
 print(resultado)
 """
 
+# Exercicio 10: Área de um Círculo
+"""
 raio_do_circulo = float(input("Digite o raio: " ))
 area_do_circulo = math.pi * raio_do_circulo ** 2
 print(f"{area_do_circulo:.2f}")
+"""
+
+# Exercicio 14: Separar Dia, Mês e Ano de uma Data
+"""
+data_usuario = input("Insira uma data no formato dd/mm/aaaa: ")
+lista_data = data_usuario.split("/")
+print(lista_data[0])
+"""
