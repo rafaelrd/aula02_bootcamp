899ef7c9aa6399774f6341e6149c60488bb697c5
rafael <rafael.ribeiro@a4pm.com.br>
2025-01-13 15:31:56 -0300

    Ajustando comentários

--- exercicios.py
+++ exercicios.py
@@ -1,13 +1,17 @@
 import math
 
 # Exercicio 04: Divisão Inteira do Primeiro pelo Segundo Número
-""" 
-numero01 = int(input("Informe um número inteiro: "))
-numero02 = int(input("Informe outro número inteiro: "))
-resultado = numero01 // numero02
-print(resultado)
-"""
-
+try:
+    numero01 = int(input("Informe um número inteiro: "))
+    numero02 = int(input("Informe outro número inteiro: "))
+    resultado = numero01 // numero02
+    print(resultado)
+except ZeroDivisionError:
+    print("Integer division or modulo by zero")
+except KeyboardInterrupt:
+    print("Acho que você não quis inserir um número")
+except: # Generico
+    print("Error")
 # Exercicio 10: Área de um Círculo
 """
 raio_do_circulo = float(input("Digite o raio: " ))
